c6634bcdd6f1efb6b67ff9c81a61fdcd155b976f
thnikk <thnikk@gmail.com>
2020-05-21 22:36:27 -0700

    Added samd21 mini and added LED effects

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,20 @@ Smaller feature goals are:
 #include <Adafruit_FreeTouch.h>
 #include <FlashAsEEPROM.h>
 #include <Bounce2.h>
-#include <Keyboard.h>
+//#include <Keyboard.h>
+#include <HID-Project.h>
 #include <FastLED.h>
-
-// Define pins for dotstar
-#define DATAPIN    7
-#define CLOCKPIN   8
 
 // Initialize inputs and LEDs
 Bounce * bounce = new Bounce[numkeys+1];
-Adafruit_FreeTouch qt = Adafruit_FreeTouch(4, OVERSAMPLE_8, RESISTOR_50K, FREQ_MODE_NONE);
+Adafruit_FreeTouch qt = Adafruit_FreeTouch(A0, OVERSAMPLE_8, RESISTOR_50K, FREQ_MODE_NONE);
 CRGBArray<numkeys> leds;
-CRGB ds[1];
+//CRGB ds[1];
 
-const byte pins[] = { 0, 2, 20, 19 };
-byte mapping[][4] = {
-{177,96,122,120},
-{218,217,216,215},
+const byte pins[] = { 12, 11 };
+uint8_t mapping[][2] = {
+{122,120},
+{177,KEY_F13}
 };
 
 bool pressed[numkeys+1];
@@ -43,6 +40,16 @@ bool lastPressed[numkeys+1];
 
 byte b = 127;
 byte bMax = 255;
+
+byte LEDmode = 3;
+byte effectSpeed = 10;
+
+// Colors for custom LED mode
+// These are the initial values stored before changed through the remapper
+byte custColor[] = { 224, 192 };
+
+// BPS
+byte bpsCount;
 
 // FreeTouch
 int touchValue;
@@ -55,17 +62,13 @@ const byte gridMap[] = {0, 1, 3, 2};
 
 void setup() {
     // Set the serial baudrate
-    Serial.begin(115200);
+    SerialUSB.begin(9600);
 
     // Start LEDs
-    FastLED.addLeds<NEOPIXEL, 1>(leds, numkeys);
-    FastLED.addLeds<DOTSTAR, INTERNAL_DS_DATA, INTERNAL_DS_CLK, BGR>(ds, 1);
+    FastLED.addLeds<NEOPIXEL, 13>(leds, numkeys);
 
     // Set brightness
     FastLED.setBrightness(255);
-
-    // Map last key to Esc
-    //mapping[numkeys] = char(177);
 
     // THIS SECTION NEEDS TO BE CHANGED FOR FULL FREETOUCH SUPPORT
 
@@ -78,7 +81,7 @@ void setup() {
 
     // Start freetouch for side button
     qt.begin();
-
+    NKROKeyboard.begin();
 }
 
 unsigned long checkMillis;
@@ -95,14 +98,14 @@ void checkState() {
         if (touchValue > touchThreshold) pressed[numkeys] = 0;
         // To release, it must go under the threshold value by an extra 50 to avoid constantly changing
         else if ( touchValue < touchThreshold-50 ) pressed[numkeys] = 1;
+#ifdef DEBUG
+        SerialUSB.print("[");
         for (byte x=0;x<=numkeys;x++) {
-            if (!pressed[x]){
-                pm = millis();
-                Serial.print("Button ");
-                Serial.print(x);
-                Serial.println("is pressed.");
-            }
+            SerialUSB.print(pressed[x]);
+            if (x != numkeys) SerialUSB.print(',');
         }
+        SerialUSB.println("]");
+#endif
         checkMillis=millis();
     }
 }
@@ -112,39 +115,128 @@ void keyboard() {
     for (byte x=0; x<numkeys; x++){
         // If the button state changes, press/release a key.
         if ( pressed[x] != lastPressed[x] ){
-            if (!pressed[x]) Keyboard.press(mapping[!pressed[numkeys]][x]);
-            if (pressed[x]) Keyboard.release(mapping[!pressed[numkeys]][x]);
+            if (!pressed[x]) bpsCount++;
+            pm = millis();
+            uint8_t key = mapping[!pressed[numkeys]][x];
+
+            // Check press state and press/release key
+            switch(key){
+                // Key exceptions need to go here for NKROKeyboard
+                case 177:
+                    if (!pressed[x]) NKROKeyboard.press(KEY_ESC);
+                    if (pressed[x]) NKROKeyboard.release(KEY_ESC);
+                    break;
+                case KEY_F13:
+                    if (!pressed[x]) NKROKeyboard.press(KEY_F13);
+                    if (pressed[x]) NKROKeyboard.release(KEY_F13);
+                    break;
+                default:
+                    if (!pressed[x]) NKROKeyboard.press(key);
+                    if (pressed[x]) NKROKeyboard.release(key);
+                    break;
+            }
+            // Save last pressed state to buffer
             lastPressed[x] = pressed[x];
         }
     }
 }
 
+// Cycle through rainbow
 void wheel(){
     static uint8_t hue;
-    bool facePress = 0;
     for(int i = 0; i < numkeys; i++) {
         byte z = gridMap[i];
-        if (pressed[i]) leds[z] = CHSV(hue,255,255);
+        if (pressed[i]) leds[z] = CHSV(hue+(i*50),255,255);
         else {
             leds[z] = 0xFFFFFF;
-            facePress = 1;
         }
     }
-    if (facePress == 0) ds[0] = CHSV(hue,255,255);
-    else ds[0] = 0xFFFFFF;
-    hue++;
+    hue--;
     FastLED.show();
 }
 
+// Fade from white to rainbow to off
+void rbFade(){
+    static int hue; // Though these are 0-255, they are saved as ints to allow overflow
+    static int sat[numkeys];
+    static int val[numkeys];
+    for(int i = 0; i < numkeys; i++) {
+        if (!pressed[i]) {
+            if (sat[i] < 255) sat[i] = sat[i]+8;
+            if (sat[i] > 255) sat[i] = 255; // Keep saturation within byte range
+            if (sat[i] == 255 && val[i] > 0) val[i] = val[i]-8;
+            if (val[i] < 0) val[i] = 0; // Same for val
+        }
+        else {
+            sat[i]=0;
+            val[i]=255;
+        }
+        byte z = gridMap[i]; // Allows custom LED mapping (for grids)
+        leds[z] = CHSV(hue+(i*50),sat[i],val[i]);
+    }
+    hue-=8;
+    if (hue < 0) hue = 255;
+    FastLED.show();
+}
+
+// Custom colors
+// Red:0, Orange:32, Yellow:64, Green:96, Aqua:128, Blue:160, Purple:192, Pink:224
+void custom(){
+    for(int i = 0; i < numkeys; i++) {
+        byte z = gridMap[i];
+        if (pressed[i]) leds[z] = CHSV(custColor[z],255,255);
+        else leds[z] = 0xFFFFFF;
+    }
+    FastLED.show();
+}
+
+unsigned long avgMillis;
+byte bpsColor;
+byte lastColor;
+void bps(){
+    // Update values once per second
+    if ((millis() - avgMillis) > 1000) {
+        lastColor = bpsColor;
+        if (bpsCount > 25) bpsCount = 25; // Max count value
+        if (bpsCount < 1) bpsCount = 1; // Prevents color jump
+        bpsColor = (bpsCount*10);
+        bpsCount = 0;
+        avgMillis = millis();
+    }
+
+    if (lastColor > bpsColor) lastColor--;
+    if (lastColor < bpsColor) lastColor++;
+
+    for(int i = 0; i < numkeys; i++) {
+        byte z = gridMap[i];
+        if (pressed[i]) leds[z] = CHSV(lastColor+100,255,255);
+        else leds[z] = 0xFFFFFF;
+    }
+    FastLED.show();
+
+}
+
 unsigned long effectMillis;
-byte effectSpeed = 10;
 void effects(byte speed) {
     // All LED modes should go here for universal speed control
     if ((millis() - effectMillis) > speed){
-        wheel();
+        // Select LED mode
+        switch(LEDmode){
+            case 0:
+                wheel(); break;
+            case 1:
+                rbFade(); break;
+            case 2:
+                custom(); break;
+            case 3:
+                bps(); break;
+        }
 
+        // Fade brightness on idle change
         if (b < bMax) b++;
         if (b > bMax) b--;
+
+        // Set brightness and global effect speed
         FastLED.setBrightness(b);
         effectMillis = millis();
     }
@@ -155,7 +247,7 @@ int count;
 void speedCheck() {
     count++;
     if ((millis() - speedCheckMillis) > 1000){
-        Serial.println(count);
+        SerialUSB.println(count);
         count = 0;
         speedCheckMillis = millis();
     }
@@ -172,15 +264,15 @@ const String menu[]={
 
 byte step;
 void mainmenu() {
-    int incomingByte = Serial.read();
+    int incomingByte = SerialUSB.read();
     step = 0;
-    if (incomingByte == 122) {
+    if (incomingByte == 48) {
         while(true){
             effects(5);
-            if (step == 0) for (byte x=0;x<4;x++) Serial.println(menu[x]);
+            if (step == 0) for (byte x=0;x<4;x++) SerialUSB.println(menu[x]);
             step = 1;
-            incomingByte = Serial.read();
-            //Serial.println(greet);
+            incomingByte = SerialUSB.read();
+            //SerialUSB.println(greet);
             if (incomingByte == 120) return;
         }
     }
@@ -189,7 +281,7 @@ void mainmenu() {
 unsigned long remapMillis;
 void serialCheck() {
     if ((millis() - remapMillis) > 10){
-        if (Serial.available() > 0) mainmenu();
+        if (SerialUSB.available() > 0) mainmenu();
         remapMillis = millis();
     }
 }
@@ -202,13 +294,12 @@ void idle(){
 void loop() {
     // Update key state to check for button presses
     checkState();
-    // Convert keypresses to actual keyboard keys
+    // Convert key presses to actual keyboard keys
     keyboard();
     // Make lights happen
     effects(10);
     idle();
     // Debug check for loops per second
-    //speedCheck();
-
+    speedCheck();
     //serialCheck();
 }
